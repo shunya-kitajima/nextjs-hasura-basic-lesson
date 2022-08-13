a2343c640794df357e58679eb1f263ea7103d5b3
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-08-13 17:22:50 +0900

    import components

---
+++ pages/local-state-b.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LocalStateB from '../components/LocalStateB'
+import Layout from '../components/Layout'
+
+const LocalStatePageB: React.FC = () => {
+  return (
+    <Layout title="Local State B">
+      <LocalStateB />
+    </Layout>
+  )
+}
+
+export default LocalStatePageB
