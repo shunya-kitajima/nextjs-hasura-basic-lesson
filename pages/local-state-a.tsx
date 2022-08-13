734d85135a352606b83a5bf983394baa78744cd9
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-08-13 17:21:12 +0900

    import components

---
+++ pages/local-state-a.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LocalStateA from '../components/LocalStateA'
+import Layout from '../components/Layout'
+
+const LocalStatePageA: React.FC = () => {
+  return (
+    <Layout title="Local State A">
+      <LocalStateA />
+    </Layout>
+  )
+}
+
+export default LocalStatePageA
