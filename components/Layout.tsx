70b7ea3281a7a654b192a31b0c156e3f12ba6a54
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-08-13 16:11:28 +0900

    create Layout component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -13,10 +13,78 @@ const Layout: React.FC<Props> = ({ children, title = 'Welcom to Nextjs' }) => {
     <div className="flex flex-col justify-center items-center min-h-screen text-gray-600 text-sm font-mono">
       <Head>
         <title>{title}</title>
-        <header>
-          <nav className="bg-gray-800 w-screen"></nav>
-        </header>
       </Head>
+      <header>
+        <nav className="bg-gray-800 w-screen">
+          <div className="flex items-center pl-8 h-14">
+            <div className="flex space-x-4">
+              <Link href="/">
+                <a
+                  data-testid="home-nav"
+                  className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded"
+                >
+                  Home
+                </a>
+              </Link>
+              <Link href="/local-state-a">
+                <a
+                  data-testid="makevar-nav"
+                  className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded"
+                >
+                  makevar
+                </a>
+              </Link>
+              <Link href="/hasura-main">
+                <a
+                  data-testid="fetchpolicy-nav"
+                  className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded"
+                >
+                  fetchPolicy(Hasura)
+                </a>
+              </Link>
+              <Link href="/hasura-crud">
+                <a
+                  data-testid="crud-nav"
+                  className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded"
+                >
+                  CRUD(Hasura)
+                </a>
+              </Link>
+              <Link href="/hasura-ssg">
+                <a
+                  data-testid="ssg-nav"
+                  className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded"
+                >
+                  SSG+ISR(Hasura)
+                </a>
+              </Link>
+              <Link href="/hooks-memo">
+                <a
+                  data-testid="memo-nav"
+                  className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded"
+                >
+                  customHook+memo
+                </a>
+              </Link>
+            </div>
+          </div>
+        </nav>
+      </header>
+      <main className="flex flex-1 flex-col justify-center items-center w-screen">
+        {children}
+      </main>
+      <footer className="w-full h-12 flex justify-center items-center border-t">
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className="flex items-center">
+            <Image src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
+          </span>
+        </a>
+      </footer>
     </div>
   )
 }
