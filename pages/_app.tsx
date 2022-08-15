13ae36ba210ad4e1dcc6a9532d0842d06adf4055
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-08-15 10:53:35 +0900

    import ApolloProvider and initializeApollo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import '../styles/globals.css'
 import { AppProps } from 'next/app'
+import { ApolloProvider } from '@apollo/client'
+import { initializeApollo } from '../lib/apolloClient'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
