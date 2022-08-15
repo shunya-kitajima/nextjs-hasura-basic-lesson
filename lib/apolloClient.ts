f9258cddba5803c8197875b117357ee63ac4b259
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-08-15 10:51:37 +0900

    add function

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -19,3 +19,14 @@ const createApolloClient = () => {
     cache: new InMemoryCache(),
   })
 }
+
+export const initializeApollo = (initialState = null) => {
+  const _apolloClient = apolloClient ?? createApolloClient()
+
+  // For SSG and SSR always create a new Apollo Client
+  if (typeof window === 'undefined') return _apolloClient
+  // Create the Apollo Client once in the client
+  if (!apolloClient) apolloClient = _apolloClient
+
+  return _apolloClient
+}
