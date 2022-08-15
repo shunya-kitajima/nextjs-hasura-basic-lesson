8bb9ab1b10c8b8a0458e12601d45593adef388e5
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-08-15 10:42:24 +0900

    add file

---
+++ lib/apolloClient.ts
@@ -0,0 +1,21 @@
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  NormalizedCacheObject,
+} from '@apollo/client'
+import 'cross-fetch/polyfill'
+
+export const APOLLI_STATE_PROP_NAME = '__APLLO_STATE__'
+
+let apolloClient: ApolloClient<NormalizedCacheObject> | undefined
+
+const createApolloClient = () => {
+  return new ApolloClient({
+    ssrMode: typeof window === 'undefined',
+    link: new HttpLink({
+      uri: 'https://hasura-basic-kita.hasura.app/v1/graphql',
+    }),
+    cache: new InMemoryCache(),
+  })
+}
