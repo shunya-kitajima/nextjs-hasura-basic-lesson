c9a2a7d58fd466c22d222c871265d407ef5f946a
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-08-16 17:19:35 +0900

    create mutations

--- pages/hasura-crud.tsx
+++ pages/hasura-crud.tsx
@@ -15,13 +15,73 @@ import {
 import Layout from '../components/Layout'
 
 const HasuraCRUD: React.FC = () => {
+  const [editedUser, setEditedUser] = useState({ id: '', name: '' })
+
   const { data, error } = useQuery<GetUsersQuery>(GET_USERS, {
     fetchPolicy: 'cache-and-network',
   })
+  const [update_users_by_pk] = useMutation<UpdateUserMutation>(UPDATE_USER)
+  const [insert_users_one] = useMutation<CreateUserMutation>(CREATE_USER, {
+    update(cache, { data: { insert_users_one } }) {
+      const cacheId = cache.identify(insert_users_one)
+      cache.modify({
+        fields: {
+          users(existingUsers, { toReference }) {
+            return [toReference(cacheId), ...existingUsers]
+          },
+        },
+      })
+    },
+  })
+  const [delete_users_by_pk] = useMutation<DeleteUserMutation>(DELETE_USER, {
+    update(cache, { data: { delete_users_by_pk } }) {
+      cache.modify({
+        fields: {
+          users(existingUsers, { readField }) {
+            return existingUsers.filter(
+              (user) => delete_users_by_pk.id !== readField('id', user)
+            )
+          },
+        },
+      })
+    },
+  })
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (editedUser.id) {
+      try {
+        await update_users_by_pk({
+          variables: {
+            id: editedUser.id,
+            name: editedUser.name,
+          },
+        })
+      } catch (err) {
+        alert(err.message)
+      }
+      setEditedUser({ id: '', name: '' })
+    } else {
+      try {
+        await insert_users_one({
+          variables: {
+            name: editedUser.name,
+          },
+        })
+      } catch (err) {
+        alert(err.message)
+      }
+      setEditedUser({ id: '', name: '' })
+    }
+  }
 
   return (
     <Layout title="HasuraCRUD">
       <p className="mb-3 font-bold">Hasura CRUD</p>
+      <form
+        className="flex flex-col justify-center"
+        onSubmit={handleSubmit}
+      ></form>
     </Layout>
   )
 }
