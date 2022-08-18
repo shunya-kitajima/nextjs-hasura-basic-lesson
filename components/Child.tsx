8dfa9ae44c02291d8034b49a8525478156599ee8
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-08-18 17:17:51 +0900

    create Child component

---
+++ components/Child.tsx
@@ -0,0 +1,22 @@
+import React, { ChangeEvent, FormEvent, memo } from 'react'
+
+interface Props {
+  printMsg: () => void
+}
+
+const Child: React.FC<Props> = ({ printMsg }) => {
+  return (
+    <>
+      {console.log('Child rendered')}
+      <p>Child Component</p>
+      <button
+        className="my-3 py-1 px-3 text-white bg-indigo-600 hover:bg-indigo-700 rounded-2xl focus:outline-none"
+        onChange={printMsg}
+      >
+        click
+      </button>
+    </>
+  )
+}
+
+export default Child
