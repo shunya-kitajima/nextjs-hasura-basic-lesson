0b56c1e7a8ea4d0cf2fd918371e827db2fc35d1d
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-08-18 17:07:56 +0900

    create CreateUser component

---
+++ components/CreateUser.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { useCreateForm } from '../hooks/useCreateForm'
+
+const CreateUser: React.FC = () => {
+  const {
+    text,
+    handleTextChange,
+    username,
+    usernameChange,
+    printMsg,
+    handleSubmit,
+  } = useCreateForm()
+
+  return (
+    <>
+      {console.log('CreateUser rendered')}
+      <p className="mb-3 font-bold">Custom Hook + useCallback + memo</p>
+      <div className="mb-3 flex flex-col justify-center items-center">
+        <label>Text</label>
+        <input
+          className="px-3 py-2 border border-gray-300"
+          type="text"
+          value={text}
+          onChange={handleTextChange}
+        />
+        <form
+          className="flex flex-col justify-center items-center"
+          onSubmit={handleSubmit}
+        >
+          <label>Username</label>
+          <input
+            className="mb-3 px-3 py-2 border border-gray-300"
+            placeholder="New user ?"
+            type="text"
+            value={username}
+            onChange={usernameChange}
+          />
+          <button
+            disabled={!username}
+            className="disabled:opacity-40 my-3 py-1 px-3 text-white bg-indigo-600 hover:bg-indigo-700 rounded-2xl focus:outline-none"
+            type="submit"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </>
+  )
+}
+
+export default CreateUser
