ac99949cff152b6de51e66631140cb1ce0e16178
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-08-18 17:21:36 +0900

    create HooksMemo component

---
+++ pages/hooks-memo.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import CreateUser from '../components/CreateUser'
+
+const HooksMemo: React.FC = () => {
+  return (
+    <Layout title="Hooks memo">
+      <CreateUser />
+    </Layout>
+  )
+}
+
+export default HooksMemo
