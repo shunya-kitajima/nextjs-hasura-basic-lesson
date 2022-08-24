ff0c7b70b45cb62d602cc4320e689f0227e29dfa
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-08-24 16:48:06 +0900

    add test find by text

--- __tests__/Nav.test.tsx
+++ __tests__/Nav.test.tsx
@@ -7,3 +7,28 @@ import userEvent from '@testing-library/user-event'
 import { getPage, initTestHelpers } from 'next-page-tester'
 import { setupServer } from 'msw/node'
 import { handlers } from '../mock/handlers'
+
+initTestHelpers()
+
+const server = setupServer(...handlers)
+
+beforeAll(() => {
+  server.listen()
+})
+afterEach(() => {
+  server.resetHandlers()
+  cleanup()
+})
+afterAll(() => {
+  server.close()
+})
+
+describe('Navigation Test Cases', () => {
+  it('Should route to selected page in navbar', async () => {
+    const { page } = await getPage({
+      route: '/',
+    })
+    render(page)
+    expect(await screen.findByText('Next.js + GraphQL')).toBeInTheDocument()
+  })
+})
