ea9342bae70ddd1803756f4663702a849a04e9cf
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-08-30 17:18:08 +0900

    modify uri

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -12,7 +12,7 @@ const createApolloClient = () => {
   return new ApolloClient({
     ssrMode: typeof window === 'undefined',
     link: new HttpLink({
-      uri: process.env.NEXT_PUBLIC_URL,
+      uri: process.env.NEXT_PUBLIC_HASURA_URL,
       headers: {
         'x-hasura-admin-secret': process.env.NEXT_PUBLIC_HASURA_KEY,
       },
