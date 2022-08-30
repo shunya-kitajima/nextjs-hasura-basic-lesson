854f54c2758825dc0e815ea25846bbb94a11e081
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-08-30 16:48:14 +0900

    add test cases

--- __tests__/HasuraSSG.test.tsx
+++ __tests__/HasuraSSG.test.tsx
@@ -3,6 +3,7 @@
  */
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen, cleanup } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { getPage, initTestHelpers } from 'next-page-tester'
 import { setupServer } from 'msw/node'
 import { handlers } from '../mock/handlers'
@@ -20,4 +21,29 @@ afterEach(() => {
 })
 afterAll(() => {
   server.close()
+})
+
+describe('SSG Test Cases', () => {
+  it('Should render the list of users pre-fetched by getStaticProps', async () => {
+    const { page } = await getPage({
+      route: '/hasura-ssg',
+    })
+    render(page)
+    expect(await screen.findByText('SSG+ISR')).toBeInTheDocument()
+    expect(screen.getByText('Test user A')).toBeInTheDocument()
+    expect(screen.getByText('Test user B')).toBeInTheDocument()
+    expect(screen.getByText('Test user C')).toBeInTheDocument()
+    userEvent.click(
+      screen.getByTestId('link-59097f69-85da-4722-b6ba-7cdd4d75cd5d')
+    )
+    expect(await screen.findByText('User detail')).toBeInTheDocument()
+    expect(
+      screen.getByText('ID :59097f69-85da-4722-b6ba-7cdd4d75cd5d')
+    ).toBeInTheDocument()
+    expect(screen.getByText('Test user A')).toBeInTheDocument()
+    expect(
+      screen.getByText('2022-08-03T06:18:56.088532+00:00')
+    ).toBeInTheDocument()
+    expect(screen.getByTestId('back-to-main')).toBeTruthy()
+  })
 })
